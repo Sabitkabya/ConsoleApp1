ccefca25c2729f7d175fceecf3463f3cbfda1a3f
gyanendra basnet <bgyanendra93@gmail.com>
2020-12-31 19:13:05 -0600

    initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+            for ( i = 0; i <=6; i++)
+            {
+                for ( j = 1; j <=7-i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.Write("\n");
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
